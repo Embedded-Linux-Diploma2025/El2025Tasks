7fe32313bf28eb1032c4f3b76f9af9a4d65e1516
SeifElkady918 <seifeldienhussein@gmail.com>
2025-07-30 22:17:42 +0200

    python_lab_3

--- python/session2/lab3_tuple_problems.py
+++ python/session2/lab3_tuple_problems.py
@@ -11,7 +11,13 @@ def tuple_operations(tuple1, tuple2):
     Returns:
         dict: Dictionary with concatenation, repetition,
     """
-    # Write your solution here
+    conc_tuple = tuple1 + tuple2
+    rep_tuple = tuple1 * 2
+    dict_result = {
+        "concatenation": conc_tuple,
+        "repetition": rep_tuple
+    }
+    return dict_result
 
 
 def find_tuple_stats(numbers_tuple):
@@ -24,6 +30,14 @@ def find_tuple_stats(numbers_tuple):
         tuple: (sum, max, min, length)
     """
     # Write your solution here
+    tsum = sum(numbers_tuple)
+    tmax = max(numbers_tuple)
+    tmin = min(numbers_tuple)
+    tlen = len(numbers_tuple)
+
+    res_tuple = (tsum, tmax, tmin, tlen)
+
+    return res_tuple
 
 
 def count_elements_in_tuple(data_tuple, element):
@@ -36,7 +50,8 @@ def count_elements_in_tuple(data_tuple, element):
     Returns:
         int: Number of occurrences
     """
-    # Write your solution here
+    num = data_tuple.count(element)
+    return num
 
 
 def tuple_indexing_slicing(data_tuple):
@@ -49,7 +64,14 @@ def tuple_indexing_slicing(data_tuple):
         dict: Dictionary with various slicing results
     """
     # Write your solution here
+    first_element = data_tuple[0]
+    last_element = data_tuple[-1]
 
+    dict_result = {
+        "first_element": first_element,
+        "last_element": last_element
+    }
+    return dict_result
 
 if __name__ == "__main__":
     # Test cases
